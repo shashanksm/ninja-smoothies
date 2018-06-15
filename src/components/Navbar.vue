d1955640db750e14f38dfe29d6a90f3858a94c2b
shashank.mutgi <shashank.mutgi@gmail.com>
2018-06-15 10:25:26 +0530

    First Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extended indigo darken-2">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title">Ninja Smoothies</span>
+                </router-link>
+                <a href="" class="btn-floating btn-large halfway-fab pink">
+                    <i class="material-icons">add</i>
+                </a>
+            </div>
+
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    name : 'Navbar',
+    data(){
+        return{
+
+        }
+    }
+
+}
+</script>
+<style>
+    .navbar{
+        position: fixed;
+        top: 0%;
+        width: 100%;
+    }
+</style>
+
